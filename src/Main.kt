f4537a887deecf3bc42f0f7bc56cb6bd5f1991a5
Bernardo Klopf <bernardoak95@gmail.com>
2026-02-10 10:14:15 -0300

    Add `Transaction` and `User` class implementations

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,12 @@
+import java.math.BigDecimal
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println(isPalindrome("racecar"))
-    println("racecar".isRealPalindrome())
-    println(isPalindrome("kotlin"))
-    println("kotlin".isRealPalindrome())
-    println(countResponseTimeRegressions(intArrayOf(100, 200, 150,300)))
-    println(findSmallestMissingPositive(intArrayOf(3, 4, -1, 1)))
-    println(isAlphabeticPalindrome("A1b2B!a"))
-    println(isAlphabeticPalindrome("A1be23B!a"))
+    val transaction = Transaction("1", BigDecimal(10.02))
+
+    println(transaction)
+    println(transaction.hashCode())
+    println(transaction.equals(transaction.copy(id = "2")))
+    println(transaction.equals(Transaction("1", BigDecimal(10.02))))
 }
